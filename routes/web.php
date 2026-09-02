7953fb8117c9cc64bcd2761ab68d7da5d266fcd3
Faza <fazaradityarma@gmail.com>
2026-09-02 09:40:52 +0700

    [P-2] implementasi route grouping prefix admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@ Route::resource('categories', CategoryController::class)->except(['show']);
 Route::resource('members', MemberController::class);
 Route::resource('loans', LoanController::class);
 Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])->name('loans.kembalikan');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Informasi Admin Panel Perpustakaan';
+    });
+});
